bbab922f79f410f036b0a54c885d0586aaf55fe4
nakednous <nakednous@gmail.com>
2026-02-10 19:44:31 -0500

    publishing test

---
+++ docs/assets/slidev/default-CFiP4jC_.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-DtEGhQT5.js";import{_ as r}from"../index-Cs3j3FI-.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
